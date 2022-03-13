6d8fe8c13e9a0a2b1de6f897fcabc9cf9bab7c61
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-03-13 16:38:16 +1300

    Fast replace between Long inserters and others #8

--- bobinserters/data-updates.lua
+++ bobinserters/data-updates.lua
@@ -1,3 +1,7 @@
+if data.raw.inserter["long-handed-inserter"] then
+  data.raw.inserter["long-handed-inserter"].fast_replaceable_group = "inserter"
+end
+
 for i, inserter in pairs(data.raw.inserter) do
   inserter.allow_custom_vectors = true
   if not inserter.hand_size then
