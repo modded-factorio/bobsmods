f055eb6fd219bc5483bc1cca5a1ca3fee623cb09
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-10-24 20:16:13 +1300

    Fixing migration file #72

--- bobwarfare/migrations/bobwarfare_1.1.6.lua
+++ bobwarfare/migrations/bobwarfare_1.1.6.lua
@@ -2,7 +2,7 @@
 for index, force in pairs(game.forces) do
 
   for techToResearch, techToCheck in pairs{
-    ["radars-2"] = "radars-1"
+    ["radars-1"] = "radars-2"
   } do
     if force.technologies[techToResearch] and force.technologies[techToCheck] then
       force.technologies[techToResearch].researched = force.technologies[techToCheck].researched or force.technologies[techToResearch].researched
