189a86fd5502ddc616f8c03da805ffd07f431bbc
Duchesko <atburghardt@gmail.com>
2022-12-08 10:45:55 +0100

    this time its right...

--- bobpower/prototypes/recipe/heat-exchangers-updates.lua
+++ bobpower/prototypes/recipe/heat-exchangers-updates.lua
@@ -1,16 +1,16 @@
 if settings.startup["bobmods-power-steam"].value == true then
-  if data.raw.item["brass-alloy"] then
-    bobmods.lib.recipe.replace_ingredient("heat-exchanger-2", "steel-plate", "brass-alloy")
-    bobmods.lib.tech.add_prerequisite("bob-heat-exchanger-2", "zinc-processing")
+  if data.raw.item["invar-alloy"] then
+    bobmods.lib.recipe.replace_ingredient("heat-exchanger-2", "steel-plate", "invar-alloy")
+    bobmods.lib.tech.add_prerequisite("bob-heat-exchanger-2", "invar-processing")
   else
-    if data.raw.item["invar-alloy"] then
-      bobmods.lib.recipe.replace_ingredient("heat-exchanger-2", "steel-plate", "invar-alloy")
-      bobmods.lib.tech.add_prerequisite("bob-heat-exchanger-2", "invar-processing")
+    if data.raw.item["brass-alloy"] then
+      bobmods.lib.recipe.replace_ingredient("heat-exchanger-2", "steel-plate", "brass-alloy")
+      bobmods.lib.tech.add_prerequisite("bob-heat-exchanger-2", "zinc-processing")
     end
   end
 
   if data.raw.item["brass-pipe"] then
-    bobmods.lib.recipe.add_ingredient("heat-exchanger-2", "brass-pipe")
+    bobmods.lib.recipe.add_ingredient("heat-exchanger-2", { "brass-pipe", 4 })
   end
   if data.raw.item["tungsten-plate"] then
     bobmods.lib.recipe.replace_ingredient("heat-exchanger-3", "steel-plate", "tungsten-plate")
