26f0cfcc7e0a774d8e567ab2a3f3952565f336a5
AiDSl <aaiddssl@gmail.com>
2022-12-27 14:37:58 +0800

    bobinserter_non_adjustable
    instead of handling inserter in the list,handling the one without new introduced property.
    this change will keep all inserter that is adjustable as is,unless they update their mod to set it true.

--- bobinserters/data-updates.lua
+++ bobinserters/data-updates.lua
@@ -2,37 +2,8 @@ if data.raw.inserter["long-handed-inserter"] then
   data.raw.inserter["long-handed-inserter"].fast_replaceable_group = "inserter"
 end
 
-local enable_inserter=
-{
-	"burner-inserter",
-	"steam-inserter",
-	
-	"inserter",
-	"yellow-filter-inserter",
-	
-	"long-handed-inserter",
-	"red-filter-inserter",
-	"red-stack-inserter",
-	"red-stack-filter-inserter",
-	
-	"fast-inserter",
-	"filter-inserter",
-	"stack-inserter",
-	"stack-filter-inserter",
-	
-	"turbo-inserter",
-	"turbo-filter-inserter",
-	"turbo-stack-inserter",
-	"turbo-stack-filter-inserter",
-	
-	"express-inserter",
-	"express-filter-inserter",
-	"express-stack-inserter",
-	"express-stack-filter-inserter",
-};
-for _,inserter_name in pairs(enable_inserter) do
-	local inserter=data.raw.inserter[inserter_name];
-	if not inserter then goto skip; end
+for _,inserter in pairs(data.raw.inserter) do
+	if inserter.bobinserter_non_adjustable then goto skip; end
 	inserter.allow_custom_vectors=true;
 	if not inserter.hand_size then  inserter.hand_size = 1.5; end
 	::skip::
