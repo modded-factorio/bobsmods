0e1718e489946384a8ce49129afd38987274e543
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-01-01 17:18:53 +1300

    Formatting

--- bobinserters/data-updates.lua
+++ bobinserters/data-updates.lua
@@ -6,9 +6,11 @@ if settings.startup["bobmods-inserters-long1"].value == true then
   end
 end
 
-for _,inserter in pairs(data.raw.inserter) do
-	if inserter.bobinserter_non_adjustable then goto skip; end
-	inserter.allow_custom_vectors=true;
-	if not inserter.hand_size then  inserter.hand_size = 1.5; end
-	::skip::
+for _, inserter in pairs(data.raw.inserter) do
+  if not inserter.bobinserter_non_adjustable then
+    inserter.allow_custom_vectors = true
+    if not inserter.hand_size then
+      inserter.hand_size = 1.5
+    end
+  end
 end
