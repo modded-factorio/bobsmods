4071f46a2960012ecee4f83983348838ac1ffca0
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-08-02 00:13:22 -0400

    AAI Industry Compatibility #133

--- bobtech/settings.lua
+++ bobtech/settings.lua
@@ -9,6 +9,11 @@ data:extend({
     type = "bool-setting",
     name = "bobmods-burnerphase",
     setting_type = "startup",
-    default_value = false,
+    default_value = true,
   },
 })
+
+if mods["aai-industry"] then
+  data.raw["bool-setting"]["bobmods-burnerphase"].hidden = true
+  data.raw["bool-setting"]["bobmods-burnerphase"].forced_value = false
+end
