8161c952cbadd4fb2ff4146e0185420bf26edd36
Qatavin <108201496+Qatavin@users.noreply.github.com>
2023-08-23 15:21:44 -0700

    Update data-final-fixes.lua for SE integration

--- bobassembly/data-final-fixes.lua
+++ bobassembly/data-final-fixes.lua
@@ -9,3 +9,18 @@ bobmods.lib.machine.copy_categories_from("furnace", "electric-furnace", "electri
 bobmods.lib.machine.copy_categories_from("furnace", "electric-furnace", "electric-furnace-3")
 bobmods.lib.machine.copy_categories_from("furnace", "electric-furnace", "multi-purpose-furnace-1")
 bobmods.lib.machine.copy_categories_from("furnace", "electric-furnace", "multi-purpose-furnace-2")
+
+if mods["space-exploration"] then
+    bobmods.lib.tech.remove_science_pack("electronics-machine-3", "se-material-science-pack-4")
+    bobmods.lib.tech.remove_science_pack("chemical-plant-3", "se-material-science-pack-1")
+    bobmods.lib.tech.remove_science_pack("chemical-plant-4", "se-material-science-pack-4")
+    if data.raw.item["chemical-plant-4"] then
+        data.raw.item["chemical-plant"].subgroup = "bob-chemical-machine"
+    end
+    if data.raw.item["oil-refinery-4"] then
+        data.raw.item["oil-refinery"].subgroup = "bob-chemical-machine"
+        data.raw.item["oil-refinery-2"].subgroup = "bob-chemical-machine"
+        data.raw.item["oil-refinery-3"].subgroup = "bob-chemical-machine"
+        data.raw.item["oil-refinery-4"].subgroup = "bob-chemical-machine"
+    end
+end
