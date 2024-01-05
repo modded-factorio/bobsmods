7ee32a60b6eccdde04b900c81f1431fba114db2e
StyLuaFormatter <KiwiHawk@users.noreply.github.com>
2024-01-05 10:04:26 +0000

    Format Code

--- boblogistics/data-final-fixes.lua
+++ boblogistics/data-final-fixes.lua
@@ -64,9 +64,10 @@ if mods["space-exploration"] then
   --Krastorio2+SE fixes
   if mods.Krastorio2 then
     data.raw.pump.pump.fast_replaceable_group = "pipe"
-    if settings.startup["bobmods-logistics-inserteroverhaul"].value == true and data.raw.inserter["long-handed-inserter"] then
+    if
+      settings.startup["bobmods-logistics-inserteroverhaul"].value == true and data.raw.inserter["long-handed-inserter"]
+    then
       data.raw.inserter["long-handed-inserter"].fast_replaceable_group = "inserter"
     end
   end
-  
 end
