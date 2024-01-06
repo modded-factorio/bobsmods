f61895973326a656aed87e0e037673543768e0c4
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2024-01-06 18:11:48 +1300

    Krastorio2 fixes #154

--- boblogistics/data-final-fixes.lua
+++ boblogistics/data-final-fixes.lua
@@ -60,14 +60,14 @@ if mods["space-exploration"] then
       { "se-energy-science-pack-2", 1 },
     }
   end
+end
 
-  --Krastorio2+SE fixes
-  if mods.Krastorio2 then
-    data.raw.pump.pump.fast_replaceable_group = "pipe"
-    if
-      settings.startup["bobmods-logistics-inserteroverhaul"].value == true and data.raw.inserter["long-handed-inserter"]
-    then
-      data.raw.inserter["long-handed-inserter"].fast_replaceable_group = "inserter"
-    end
+-- Krastorio2 fixes
+if mods["Krastorio2"] then
+  data.raw.pump.pump.fast_replaceable_group = "pipe"
+  if
+    settings.startup["bobmods-logistics-inserteroverhaul"].value == true and data.raw.inserter["long-handed-inserter"]
+  then
+    data.raw.inserter["long-handed-inserter"].fast_replaceable_group = "inserter"
   end
 end
