62944a21232b7787beef83a6837d86efb6277a96
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-01-06 16:57:49 -0800

    Move conditional on require function to inside file

--- bobequipment/data-updates.lua
+++ bobequipment/data-updates.lua
@@ -1,6 +1,4 @@
 require("prototypes.recipe.updates")
 require("prototypes.technology.technology-updates")
 
-if mods["space-exploration"] then
-  require("prototypes.se-updates")
-end
+require("prototypes.se-updates")
