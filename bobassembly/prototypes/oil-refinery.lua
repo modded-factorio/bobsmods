884bdd777f3a168d47133bc226e8d6d932a26767
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-10-25 21:39:27 -0700

    Update pollution and renamed tech prereqs

--- bobassembly/prototypes/oil-refinery.lua
+++ bobassembly/prototypes/oil-refinery.lua
@@ -52,7 +52,7 @@ if settings.startup["bobmods-assembly-oilrefineries"].value == true then
         module_slots = 4,
         crafting_speed = 1.75,
         energy_usage = "720kW",
-        energy_source = { emissions_per_minute = 5 },
+        energy_source = { emissions_per_minute = { pollution = 5 } },
       },
     }),
 
@@ -63,7 +63,7 @@ if settings.startup["bobmods-assembly-oilrefineries"].value == true then
       icon_size = 256,
       prerequisites = {
         "oil-processing",
-        "advanced-electronics",
+        "advanced-circuit",
         "chemical-science-pack",
       },
       order = "d-a2",
@@ -125,7 +125,7 @@ if settings.startup["bobmods-assembly-oilrefineries"].value == true then
         module_slots = 5,
         crafting_speed = 2.5,
         energy_usage = "1MW",
-        energy_source = { emissions_per_minute = 4 },
+        energy_source = { emissions_per_minute = { pollution = 4 } },
       },
     }),
 
@@ -136,7 +136,7 @@ if settings.startup["bobmods-assembly-oilrefineries"].value == true then
       icon_size = 256,
       prerequisites = {
         "oil-processing-2",
-        "advanced-electronics-2",
+        "processing-unit",
         "production-science-pack",
       },
       order = "d-a3",
@@ -198,7 +198,7 @@ if settings.startup["bobmods-assembly-oilrefineries"].value == true then
         module_slots = 6,
         crafting_speed = 3.5,
         energy_usage = "1.35MW",
-        energy_source = { emissions_per_minute = 3 },
+        energy_source = { emissions_per_minute = { pollution = 3 } },
       },
     }),
 
