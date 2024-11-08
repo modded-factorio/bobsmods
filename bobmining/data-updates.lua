366b6168b3962f0d64b8a5eae39d6eaefa2ecf02
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-11-07 19:54:59 -0800

    Replace vanilla electric mining drill icon

--- bobmining/data-updates.lua
+++ bobmining/data-updates.lua
@@ -2,3 +2,6 @@ require("prototypes.drill-updates")
 require("prototypes.areadrill-updates")
 require("prototypes.pumpjack-updates")
 require("prototypes.water-miner-updates")
+
+data.raw.item["electric-mining-drill"].icon = "__bobmining__/graphics/icons/electric-mining-drill-1.png"
+data.raw["mining-drill"]["electric-mining-drill"].icon = "__bobmining__/graphics/icons/electric-mining-drill-1.png"
