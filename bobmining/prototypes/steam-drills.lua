ac15fa696c87c9b2f0d732eb142ad276c9d787cf
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-11-25 22:04:22 -0800

    Bobmining2.0 (#214)
    * Add mining drill color masks
    
    * Add new mining drill icons
    
    * Update water-miner.lua
    
    Replace animation with graphics_set, update fluid boxes, pollution, and circuit connections, add next_upgrade, remove old vehicle_impact_sound,
    
    * Update steam-drills.lua
    
    Update fluid boxes and pollution, remove references to hr_version
    
    * Update pumpjacks.lua
    
    Update fluid boxes, pollution, and circuit connections. Remove lowres graphics. Add next_upgrade. Remove old vehicle_impact_sound.
    
    * Change "true" to true
    
    * Update drills.lua
    
    Update pollution, add new icons, add next_upgrade, add new tint.
    
    * Update areadrills.lua
    
    Update pollution, add new icons, add next_upgrade, add new tint.
    
    Pollution also adjusted to match regular mining drills of the same tier.
    
    * Update drill-functions.lua
    
    Added color mask functions, added ability to customize icon and next_upgrade
    
    * Replace vanilla electric mining drill icon
    
    * Add new item sounds
    
    * Add new item sounds
    
    * Add new item sounds
    
    * Add new item sounds
    
    * Add new item sounds

--- bobmining/prototypes/steam-drills.lua
+++ bobmining/prototypes/steam-drills.lua
@@ -9,6 +9,18 @@ if settings.startup["bobmods-mining-steamminingdrills"].value == true then
       order = "a[items]-a[steam-mining-drill]",
       place_result = "steam-mining-drill",
       stack_size = 50,
+      drop_sound = {
+        filename = "__base__/sound/item/drill-inventory-move.ogg",
+        volume = 0.8
+      },
+      inventory_move_sound = {
+        filename = "__base__/sound/item/drill-inventory-move.ogg",
+        volume = 0.8
+      },
+      pick_sound = {
+        filename = "__base__/sound/item/drill-inventory-pickup.ogg",
+        volume = 0.8
+      },
     },
 
     {
@@ -45,14 +57,12 @@ if settings.startup["bobmods-mining-steamminingdrills"].value == true then
   data.raw["mining-drill"]["steam-mining-drill"].energy_source = {
     type = "fluid",
     effectivity = 1,
-    emissions_per_minute = 25, --fairly sure this scales, so it would be 5 at level 1 speed.
+    emissions_per_minute = {pollution = 25}, --fairly sure this scales, so it would be 5 at level 1 speed.
     fluid_box = {
-      base_area = 1,
-      height = 2,
-      base_level = -1,
+      volume = 200,
       pipe_connections = {
-        { type = "input-output", position = { 1.5, 0.5 } },
-        { type = "input-output", position = { -1.5, 0.5 } },
+        { flow_direction = "input-output", position = { 0.5, 0.5 }, direction = defines.direction.east },
+        { flow_direction = "input-output", position = { -0.5, 0.5 }, direction = defines.direction.west },
       },
       pipe_covers = pipecoverspictures(),
       pipe_picture = assembler2pipepictures(),
@@ -75,10 +85,9 @@ if settings.startup["bobmods-mining-steamminingdrills"].value == true then
   }
 
   --Change all the animation speeds from 0.5 to 4 (mining speed increases from 0.25 to 2)
-  for i, direction in pairs(data.raw["mining-drill"]["steam-mining-drill"].animations) do
+  for i, direction in pairs(data.raw["mining-drill"]["steam-mining-drill"].graphics_set.animation) do
     for j, layer in pairs(direction.layers) do
       layer.animation_speed = 4
-      layer.hr_version.animation_speed = 4
     end
   end
 end
