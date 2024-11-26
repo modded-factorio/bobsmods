e4621f6feb523682878e509abb96d7a9a690781c
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-11-26 05:53:16 -0800

    Update aluminium and titanium item names

--- boblogistics/prototypes/recipe/inserter-recipe-updates.lua
+++ boblogistics/prototypes/recipe/inserter-recipe-updates.lua
@@ -12,7 +12,7 @@ if settings.startup["bobmods-logistics-inserteroverhaul"].value == true then
 
   if data.raw.item["bob-aluminium-plate"] then
     bobmods.lib.recipe.replace_ingredient("fast-inserter", "steel-plate", "bob-aluminium-plate")
-    bobmods.lib.recipe.replace_ingredient("bulk-inserter", "steel-plate", "bob-aluminium-plate")
+    bobmods.lib.recipe.replace_ingredient("stack-inserter", "steel-plate", "bob-aluminium-plate")
     if settings.startup["bobmods-logistics-inserterrequireprevious"].value == true then
       bobmods.lib.recipe.replace_ingredient("filter-inserter", "steel-plate", "bob-aluminium-plate")
       bobmods.lib.recipe.replace_ingredient("stack-filter-inserter", "steel-plate", "bob-aluminium-plate")
@@ -56,7 +56,7 @@ if settings.startup["bobmods-logistics-inserteroverhaul"].value == true then
 
   if data.raw.item["bob-titanium-plate"] then
     bobmods.lib.recipe.replace_ingredient("turbo-inserter", "steel-plate", "bob-titanium-plate")
-    bobmods.lib.recipe.replace_ingredient("turbo-bulk-inserter", "steel-plate", "bob-titanium-plate")
+    bobmods.lib.recipe.replace_ingredient("turbo-stack-inserter", "steel-plate", "bob-titanium-plate")
     if settings.startup["bobmods-logistics-inserterrequireprevious"].value == true then
       bobmods.lib.recipe.replace_ingredient("turbo-filter-inserter", "steel-plate", "bob-titanium-plate")
       bobmods.lib.recipe.replace_ingredient("turbo-stack-filter-inserter", "steel-plate", "bob-titanium-plate")
