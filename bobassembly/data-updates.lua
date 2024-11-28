3dbb8337858c57d48914d06cca9e55fab1299c8c
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-11-27 16:27:45 -0800

    Add function call for update_recycling_recipe
    This has been tested, and it works properly in the data-updates stage. However, because the Quality mod generates recycling recipes in this stage as well, there may be other cases where this function call will need to be in data-final-fixes instead.

--- bobassembly/data-updates.lua
+++ bobassembly/data-updates.lua
@@ -16,3 +16,38 @@ data.raw.fluid["heavy-oil"].emissions_multiplier = 3
 data.raw.fluid["light-oil"].fuel_value = "1.5MJ"
 data.raw.fluid["light-oil"].emissions_multiplier = 2
 data.raw.fluid["petroleum-gas"].fuel_value = "2.3MJ"
+
+if feature_flags["quality"] then
+  bobmods.lib.recipe.update_recycling_recipe({
+    "assembling-machine-3",
+    "assembling-machine-4",
+    "assembling-machine-5",
+    "assembling-machine-6",
+    "electronics-machine-1",
+    "electronics-machine-2",
+    "electronics-machine-3",
+    "centrifuge-2",
+    "centrifuge-3",
+    "electric-chemical-mixing-furnace",
+    "electric-chemical-mixing-furnace-2",
+    "chemical-plant-2",
+    "chemical-plant-3",
+    "chemical-plant-4",
+    "bob-distillery",
+    "bob-distillery-2",
+    "bob-distillery-3",
+    "bob-distillery-4",
+    "bob-distillery-5",
+    "electric-furnace-2",
+    "electric-furnace-3",
+    "electrolyser-2",
+    "electrolyser-3",
+    "electrolyser-4",
+    "electrolyser-5",
+    "fluid-mixing-furnace",
+    "fluid-chemical-furnace",
+    "oil-refinery-2",
+    "oil-refinery-3",
+    "oil-refinery-4",
+  })
+end
