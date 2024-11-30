51fd08712999fe77a936f6545d69ed0109b35803
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-11-29 19:58:32 -0800

    Add function call for updating recycling recipes

--- bobgreenhouse/prototypes/updates.lua
+++ bobgreenhouse/prototypes/updates.lua
@@ -29,3 +29,8 @@ if data.raw.technology["nitrogen-processing"] then
 else
   bobmods.lib.tech.add_prerequisite("bob-fertiliser", "sulfur-processing")
 end
+
+if feature_flags["quality"] then
+  bobmods.lib.recipe.update_recycling_recipe_single("bob-greenhouse", false)
+  bobmods.lib.recipe.update_recycling_recipe_to_self_recipe("wood-pellets", false)
+end
