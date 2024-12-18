184fa0a5f5c8637d755212aa1cbb340fb26f8d45
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-12-18 02:53:48 -0800

    Add function call for update_recycling_recipe (#245)
    * Add function call for update_recycling_recipe
    
    This has been tested, and it works properly in the data-updates stage. However, because the Quality mod generates recycling recipes in this stage as well, there may be other cases where this function call will need to be in data-final-fixes instead.
    
    * Add function call for update_recycling_recipe
    
    * Expand function call for update_recycling_recipe
    
    * Add function call for updating recycling recipes
    
    * Add function call for update_recycling_recipe
    
    * Add function call for updating recycling recipes
    
    * Add to update_recycling_recipe list
    
    * Add function call for update_recycling_recipe
    
    * Add function call for updating recycling recipes
    
    * Add function call for update_recycling_recipe
    
    * Add function calls for updating recycling recipes and icons
    
    ---------
    
    Co-authored-by: KiwiHawk <59639+KiwiHawk@users.noreply.github.com>

--- bobtech/data-updates.lua
+++ bobtech/data-updates.lua
@@ -15,4 +15,14 @@ if settings.startup["bobmods-tech-colorupdate"].value == true then
   data.raw.technology["automation-science-pack"].icon = "__base__/graphics/technology/utility-science-pack.png"
   data.raw.technology["logistic-science-pack"].icon = "__base__/graphics/technology/automation-science-pack.png"
   data.raw.technology["utility-science-pack"].icon = "__base__/graphics/technology/logistic-science-pack.png"
+  
+  if feature_flags["quality"] then
+    bobmods.lib.recipe.update_recycling_recipe_icon("automation-science-pack-recycling", "__base__/graphics/icons/utility-science-pack.png")
+    bobmods.lib.recipe.update_recycling_recipe_icon("logistic-science-pack-recycling", "__base__/graphics/icons/automation-science-pack.png")
+    bobmods.lib.recipe.update_recycling_recipe_icon("utility-science-pack-recycling", "__base__/graphics/icons/logistic-science-pack.png")
+  end
+end
+
+if feature_flags["quality"] then
+  bobmods.lib.recipe.update_recycling_recipe_single("lab")
 end
