5b024cee130f114b2056cf037f6a891a8e1ea8bd
Qatavin <108201496+Qatavin@users.noreply.github.com>
2024-12-18 20:26:21 -0800

    Add burner generator

--- bobtech/prototypes/entity/entity.lua
+++ bobtech/prototypes/entity/entity.lua
@@ -293,4 +293,96 @@ if settings.startup["bobmods-burnerphase"].value == true then
   if mods["DiscoScience"] and DiscoScience and DiscoScience.prepareLab then
     DiscoScience.prepareLab(data.raw["lab"]["burner-lab"])
   end
+
+  if not data.raw["burner-generator"]["bob-burner-generator"] then
+    data:extend({
+      {
+        type = "burner-generator",
+        name = "bob-burner-generator",
+        icon = "__bobtech__/graphics/icons/burner-generator.png",
+        icon_size = 64,
+        flags = { "placeable-neutral", "player-creation" },
+        minable = { mining_time = 0.5, result = "bob-burner-generator" },
+        max_health = 200,
+        corpse = "big-remnants",
+        dying_explosion = "medium-explosion",
+        max_power_output = "400kW",
+        resistances = {
+          {
+            type = "fire",
+            percent = 70,
+          },
+          {
+            type = "impact",
+            percent = 30,
+          },
+        },
+        fast_replaceable_group = "burner-generator",
+        collision_box = { { -1.35, -1.35 }, { 1.35, 1.35 } },
+        selection_box = { { -1.5, -1.5 }, { 1.5, 1.5 } },
+        energy_source = {
+          type = "electric",
+          usage_priority = "secondary-output",
+        },
+        burner = {
+          type = "burner",
+          fuel_categories = { "chemical" },
+          effectivity = 0.75,
+          fuel_inventory_size = 1,
+          emissions_per_minute = { pollution = 15 },
+          smoke = {
+            {
+              name = "smoke",
+              position = util.by_pixel(0, -50),
+              frequency = 15,
+              starting_vertical_speed = 0.0,
+              starting_frame_deviation = 60,
+            },
+          },
+        },
+        idle_animation = {
+          filename = "__bobtech__/graphics/burner-generator/burner-generator.png",
+          priority = "extra-high",
+          width = 180,
+          height = 190,
+          frame_count = 1,
+          repeat_count = 12,
+          shift = util.by_pixel(40, 12),
+        },
+        animation = {
+          layers = {
+            {
+              filename = "__bobtech__/graphics/burner-generator/burner-generator.png",
+              priority = "extra-high",
+              width = 180,
+              height = 190,
+              frame_count = 1,
+              repeat_count = 12,
+              shift = util.by_pixel(40, 12),
+            },
+            {
+              filename = "__bobtech__/graphics/burner-generator/burner-generator-fire.png",
+              priority = "high",
+              width = 36,
+              height = 19,
+              frame_count = 12,
+              direction_count = 1,
+              shift = util.by_pixel(2, 37.5),
+            },
+          },
+        },
+        impact_category = "metal-large",
+        working_sound = {
+          sound = {
+            filename = "__base__/sound/steam-engine-90bpm.ogg",
+            volume = 0.6,
+          },
+          match_speed_to_activity = true,
+        },
+        min_perceived_performance = 0.25,
+        performance_to_sound_speedup = 0.5,
+      },
+    })
+  end
+
 end
