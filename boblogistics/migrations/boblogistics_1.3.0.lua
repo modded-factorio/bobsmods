fbf9516ac6a67b25ae475580cca173deff2b9e9f
StyLuaFormatter <KiwiHawk@users.noreply.github.com>
2024-12-30 05:35:39 +0000

    Format Code

--- boblogistics/migrations/boblogistics_1.3.0.lua
+++ boblogistics/migrations/boblogistics_1.3.0.lua
@@ -4,7 +4,9 @@
 -- Bob's valves are replaced with new valves in json migration
 -- However they will be facing the wrong direction so need to be removed and recreated
 
-if not script.active_mods["valves"] then return end
+if not script.active_mods["valves"] then
+  return
+end
 
 local util = require("util")
 
@@ -17,21 +19,21 @@ local replace_behaviour = {
 for _, surface in pairs(game.surfaces) do
   for _, entity_name in pairs(replace_behaviour) do
     if prototypes.entity[entity_name] then
-      for _, entity in pairs(surface.find_entities_filtered{name = entity_name}) do
+      for _, entity in pairs(surface.find_entities_filtered({ name = entity_name })) do
         local position = entity.position
         local direction = entity.direction
         local force = entity.force
-        entity.destroy{raise_destroy = true}
+        entity.destroy({ raise_destroy = true })
 
         direction = util.oppositedirection(direction)
 
-        local valve = surface.create_entity{
+        local valve = surface.create_entity({
           name = entity_name,
           position = position,
           force = force,
           direction = direction,
           raise_built = true,
-        }
+        })
       end
     end
   end
