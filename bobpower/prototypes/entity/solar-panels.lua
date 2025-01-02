180d06d5b8bc3db3392e501aeadcca8e10b5c99f
StyLuaFormatter <KiwiHawk@users.noreply.github.com>
2025-01-02 01:08:04 +0000

    Format Code

--- bobpower/prototypes/entity/solar-panels.lua
+++ bobpower/prototypes/entity/solar-panels.lua
@@ -1,17 +1,16 @@
 if settings.startup["bobmods-power-solar"].value == true then
-
   -- Copied from reskins-library with permission
 
   ---@type data.Color[]
   local standard_tier_colors = {
-      ---@type data.Color
-      [0] = util.color("808080"), -- 1.1.7: 4d4d4d
-      [1] = util.color("ffb726"), -- 1.1.7: de9400
-      [2] = util.color("f22318"), -- 1.1.7: c20600
-      [3] = util.color("33b4ff"), -- 1.1.7: 0099ff, 1.1.0: 1b87c2
-      [4] = util.color("b459ff"), -- 1.1.7: a600bf
-      [5] = util.color("2ee55c"), -- 1.1.7: 16c746, 1.1.6: 23de55
-      [6] = util.color("ff8533"), -- 1.1.7: ff7700
+    ---@type data.Color
+    [0] = util.color("808080"), -- 1.1.7: 4d4d4d
+    [1] = util.color("ffb726"), -- 1.1.7: de9400
+    [2] = util.color("f22318"), -- 1.1.7: c20600
+    [3] = util.color("33b4ff"), -- 1.1.7: 0099ff, 1.1.0: 1b87c2
+    [4] = util.color("b459ff"), -- 1.1.7: a600bf
+    [5] = util.color("2ee55c"), -- 1.1.7: 16c746, 1.1.6: 23de55
+    [6] = util.color("ff8533"), -- 1.1.7: ff7700
   }
 
   local solar_panel = data.raw["solar-panel"]["solar-panel"]
@@ -72,7 +71,6 @@ if settings.startup["bobmods-power-solar"].value == true then
       },
     },
   }
-
 
   data:extend({
     {
