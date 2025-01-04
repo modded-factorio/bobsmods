77696b98179111c3b5a17d5157a6d1bfd4d8400b
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-01-03 18:53:08 -0800

    Shrink equipment grid sizes

--- bobwarfare/prototypes/equipment-grid.lua
+++ bobwarfare/prototypes/equipment-grid.lua
@@ -2,22 +2,22 @@ data:extend({
   {
     type = "equipment-grid",
     name = "power-armor-equipment-grid-mk3",
-    width = 12,
+    width = 10,
     height = 12,
     equipment_categories = { "armor" },
   },
   {
     type = "equipment-grid",
     name = "power-armor-equipment-grid-mk4",
-    width = 14,
-    height = 14,
+    width = 11,
+    height = 13,
     equipment_categories = { "armor" },
   },
   {
     type = "equipment-grid",
     name = "power-armor-equipment-grid-mk5",
-    width = 16,
-    height = 16,
+    width = 12,
+    height = 14,
     equipment_categories = { "armor" },
   },
 })
