0e04502446c1e9da63930d099640f099d45db91b
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-01-05 17:22:38 -0800

    Remove old constant icon function

--- bobequipment/data.lua
+++ bobequipment/data.lua
@@ -5,20 +5,6 @@ if not bobmods.equipment then
   bobmods.equipment = {}
 end
 
-function bobmods.equipment.technology_icon_constant_equipment(technology_icon)
-  local scale = 0.5
-  local icons = {
-    technology_icon,
-    {
-      icon = "__bobequipment__/graphics/icons/technology/constant-equipment.png",
-      icon_size = 104,
-      scale = scale,
-      shift = { 48 * scale, 76 * scale },
-    },
-  }
-  return icons
-end
-
 require("prototypes.item.equipment")
 require("prototypes.recipe.equipment")
 require("prototypes.technology.equipment")
