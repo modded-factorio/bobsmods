7446ec687ed59cf81923249afe4a0a8bda2a214f
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-01-30 21:22:34 -0800

    Update data.lua

--- bobenemies/data.lua
+++ bobenemies/data.lua
@@ -7,12 +7,12 @@ end
 
 data.raw["noise-expression"].enemy_base_intensity.expression = "clamp(distance, 0, 1600) / 325"
 
-data.raw["map-settings"]["map-settings"].enemy_evolution.time_factor= 8e-07
-data.raw["map-settings"]["map-settings"].enemy_evolution.destroy_factor= 0.0008
+data.raw["map-settings"]["map-settings"].enemy_evolution.time_factor= 4e-07
+data.raw["map-settings"]["map-settings"].enemy_evolution.destroy_factor= 0.0004
 data.raw["map-settings"]["map-settings"].enemy_evolution.pollution_factor= 6e-07
-data.raw["map-settings"]["map-settings"].enemy_expansion.min_expansion_cooldown = 60 * 60 * 8
+data.raw["map-settings"]["map-settings"].enemy_expansion.min_expansion_cooldown = 60 * 60 * 4
 data.raw["map-settings"]["map-settings"].enemy_expansion.max_expansion_cooldown = 60 * 60 * 15
-data.raw["map-settings"]["map-settings"].enemy_expansion.settler_group_min_size = 12
+data.raw["map-settings"]["map-settings"].enemy_expansion.settler_group_min_size = 15
 data.raw["map-settings"]["map-settings"].enemy_expansion.settler_group_max_size = 30
 data.raw["map-settings"]["map-settings"].pollution.ageing = 0.4
 data.raw["map-settings"]["map-settings"].pollution.diffusion_ratio = 0.1
