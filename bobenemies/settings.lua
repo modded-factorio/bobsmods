f54904a6758ccc5b823a38811c0018c5af161a7d
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-01-30 21:23:07 -0800

    Update settings.lua

--- bobenemies/settings.lua
+++ bobenemies/settings.lua
@@ -42,7 +42,7 @@ data:extend({
     type = "bool-setting",
     name = "bobmods-enemies-superspawner",
     setting_type = "startup",
-    default_value = false,
+    default_value = true,
   },
   {
     type = "bool-setting",
@@ -50,4 +50,31 @@ data:extend({
     setting_type = "startup",
     default_value = false,
   },
+  {
+    type = "bool-setting",
+    name = "bobmods-enemies-originalcolors",
+    setting_type = "startup",
+    default_value = false,
+  },
+  {
+    type = "double-setting",
+    name = "bobmods-enemies-factionsappear",
+    setting_type = "startup",
+    default_value = 0.5,
+    allowed_values = { 0, 0.2, 0.3, 0.4, 0.5, 0.6 },
+  },
+  {
+    type = "bool-setting",
+    name = "bobmods-enemies-qualityenemies",
+    setting_type = "startup",
+    default_value = false,
+    hidden = true,
+    forced_value = false,
+  },
 })
+
+if feature_flags["quality"] then
+  data.raw["bool-setting"]["bobmods-enemies-qualityenemies"].hidden = false
+  data.raw["bool-setting"]["bobmods-enemies-qualityenemies"].forced_value = nil
+  data.raw["bool-setting"]["bobmods-enemies-qualityenemies"].default_value = true
+end
