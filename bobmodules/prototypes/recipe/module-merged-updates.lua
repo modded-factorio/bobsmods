0b11316072fb6fc0fce3281651b3bf3cb465b914
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-02-22 02:00:40 -0800

    Update module-merged-updates.lua

--- bobmodules/prototypes/recipe/module-merged-updates.lua
+++ bobmodules/prototypes/recipe/module-merged-updates.lua
@@ -53,7 +53,7 @@ if settings.startup["bobmods-modules-enablegodmodules"].value == true then
 
   if mods["quality"] then
     if data.raw.item["bob-alien-artifact-red"] then
-      bobmods.lib.recipe.add_ingredient("bob-god-module-productivity", { type = "item", name = "bob-alien-artifact", amount = 7 })
+      bobmods.lib.recipe.add_ingredient("bob-god-module-quality", { type = "item", name = "bob-alien-artifact", amount = 7 })
       bobmods.lib.recipe.add_ingredient("bob-god-module-quality", { type = "item", name = "bob-alien-artifact-blue", amount = 7 })
       bobmods.lib.recipe.add_ingredient("bob-god-module-quality", { type = "item", name = "bob-alien-artifact-orange", amount = 7 })
       bobmods.lib.recipe.add_ingredient("bob-god-module-quality", { type = "item", name = "bob-alien-artifact-purple", amount = 7 })
