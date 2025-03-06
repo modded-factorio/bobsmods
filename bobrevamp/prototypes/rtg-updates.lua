b488f47125c86991f5bb79290a8600eada44b27d
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-03-06 22:20:30 +1300

    Rename ammonium chloride recycling recipe so it doesn't clash with recycling

--- bobrevamp/prototypes/rtg-updates.lua
+++ bobrevamp/prototypes/rtg-updates.lua
@@ -10,9 +10,9 @@ if bobmods.plates and settings.startup["bobmods-revamp-rtg"].value == true then
         { type = "fluid", name = "bob-pure-water", amount = 10, fluidbox_index = 1, ignored_by_productivity = 10 }
       )
 
-      bobmods.lib.recipe.remove_result("bob-ammonium-chloride-recycling", "water")
+      bobmods.lib.recipe.remove_result("bob-ammonium-chloride-reprocessing", "water")
       bobmods.lib.recipe.add_result(
-        "bob-ammonium-chloride-recycling",
+        "bob-ammonium-chloride-reprocessing",
         { type = "fluid", name = "bob-pure-water", amount = 10 }
       )
 
@@ -24,6 +24,6 @@ if bobmods.plates and settings.startup["bobmods-revamp-rtg"].value == true then
     bobmods.lib.tech.add_recipe_unlock("bob-rtg", "bob-brine")
     bobmods.lib.tech.add_recipe_unlock("bob-rtg", "bob-ammoniated-brine")
     bobmods.lib.tech.add_recipe_unlock("bob-rtg", "bob-sodium-bicarbonate")
-    bobmods.lib.tech.add_recipe_unlock("bob-rtg", "bob-ammonium-chloride-recycling")
+    bobmods.lib.tech.add_recipe_unlock("bob-rtg", "bob-ammonium-chloride-reprocessing")
   end
 end
