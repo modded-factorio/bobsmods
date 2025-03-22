09a029928d73663a54a4b3f185ff7aea694a23f2
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-03-23 10:19:55 +1300

    Removing unused item group/subgroups

--- bobplates/prototypes/category.lua
+++ bobplates/prototypes/category.lua
@@ -35,56 +35,6 @@ data:extend({
     type = "recipe-category",
     name = "bob-distillery",
   },
-
-  {
-    type = "item-group",
-    name = "bob-logistics",
-    order = "aaa-a",
-    icon = "__bobplates__/graphics/icons/technology/logistics.png",
-    icon_size = 128,
-  },
-  {
-    type = "item-subgroup",
-    name = "bob-logistic-robots",
-    group = "bob-logistics",
-    order = "f-a",
-  },
-  {
-    type = "item-subgroup",
-    name = "bob-logistic-roboport",
-    group = "bob-logistics",
-    order = "f-b",
-  },
-  {
-    type = "item-subgroup",
-    name = "bob-energy-boiler",
-    group = "production",
-    order = "b-a",
-  },
-  {
-    type = "item-subgroup",
-    name = "bob-energy-steam-engine",
-    group = "production",
-    order = "b-b",
-  },
-  {
-    type = "item-subgroup",
-    name = "bob-energy-solar-panel",
-    group = "production",
-    order = "b-c",
-  },
-  {
-    type = "item-subgroup",
-    name = "bob-energy-accumulator",
-    group = "production",
-    order = "b-d",
-  },
-  {
-    type = "item-subgroup",
-    name = "bob-extraction-machine",
-    group = "production",
-    order = "c-a",
-  },
   {
     type = "item-subgroup",
     name = "bob-pump",
