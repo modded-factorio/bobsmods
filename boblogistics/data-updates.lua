0c5d477390358144ca5ebfc6b3dc2e09e5e1c8c2
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-03-29 16:54:42 -0700

    Update data-updates.lua

--- boblogistics/data-updates.lua
+++ boblogistics/data-updates.lua
@@ -172,6 +172,40 @@ bobmods.lib.item.set_subgroup("express-splitter", "bob-logistic-tier-3")
 -- because there's no specific section for it.
 data.raw["pump"]["pump"].next_upgrade = "bob-pump-2"
 
+if mods["aai-containers"] then
+  data.raw.item["passive-provider-chest"].order = "b[storage]-b"
+  data.raw.item["active-provider-chest"].order = "b[storage]-c"
+  data.raw.item["storage-chest"].order = "b[storage]-d"
+  data.raw.item["buffer-chest"].order = "b[storage]-e"
+  data.raw.item["requester-chest"].order = "b[storage]-f"
+  data.raw["item-subgroup"]["bob-logistic-chests-2"].order = "a1[container-1]-a"
+  data.raw["item-subgroup"]["bob-logistic-chests-3"].order = "a1[container-1]-b"
+  if mods["bobplates"] then
+    data.raw.item["bob-brass-chest"].subgroup = "bob-logistic-chests-2"
+    data.raw.item["bob-titanium-chest"].subgroup = "bob-logistic-chests-3"
+  end
+  data.raw.item["bob-passive-provider-chest-2"].order = "b[storage]-b2"
+  data.raw.item["bob-active-provider-chest-2"].order = "b[storage]-c2"
+  data.raw.item["bob-storage-chest-2"].order = "b[storage]-d2"
+  data.raw.item["bob-buffer-chest-2"].order = "b[storage]-e2"
+  data.raw.item["bob-requester-chest-2"].order = "b[storage]-f2"
+  data.raw.item["bob-passive-provider-chest-3"].order = "b[storage]-b3"
+  data.raw.item["bob-active-provider-chest-3"].order = "b[storage]-c3"
+  data.raw.item["bob-storage-chest-3"].order = "b[storage]-d3"
+  data.raw.item["bob-buffer-chest-3"].order = "b[storage]-e3"
+  data.raw.item["bob-requester-chest-3"].order = "b[storage]-f3"
+end
+if mods["aai-programmable-vehicles"] then
+  data.raw["item-subgroup"]["ai-vehicles"].group = "bob-logistics"
+  data.raw["item-subgroup"]["ai-vehicles-reverse"].group = "bob-logistics"
+  data.raw["item-subgroup"]["ai-vehicles"].order = "h2"
+  data.raw["item-subgroup"]["ai-vehicles-reverse"].order = "h3"
+end
+if mods["aai-programmable-structures"] then
+  data.raw["item-subgroup"]["programmable-structures"].group = "bob-logistics"
+  data.raw["item-subgroup"]["programmable-structures"].order = "h1"
+end
+
 if mods["quality"] then
   bobmods.lib.recipe.update_recycling_recipe({
     "transport-belt",
