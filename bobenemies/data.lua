21866fcb7ce6089416792a5132b9dc3f49661335
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-03-31 20:03:22 -0700

    Update data.lua

--- bobenemies/data.lua
+++ bobenemies/data.lua
@@ -10,12 +10,10 @@ data.raw["noise-expression"].enemy_base_intensity.expression = "clamp(distance, 
 data.raw["map-settings"]["map-settings"].enemy_evolution.time_factor = 4e-07
 data.raw["map-settings"]["map-settings"].enemy_evolution.destroy_factor = 0.0004
 data.raw["map-settings"]["map-settings"].enemy_evolution.pollution_factor = 6e-07
-data.raw["map-settings"]["map-settings"].enemy_expansion.min_expansion_cooldown = 60 * 60 * 4
+data.raw["map-settings"]["map-settings"].enemy_expansion.min_expansion_cooldown = 60 * 60 * 6
 data.raw["map-settings"]["map-settings"].enemy_expansion.max_expansion_cooldown = 60 * 60 * 15
 data.raw["map-settings"]["map-settings"].enemy_expansion.settler_group_min_size = 15
 data.raw["map-settings"]["map-settings"].enemy_expansion.settler_group_max_size = 30
-data.raw["map-settings"]["map-settings"].pollution.ageing = 0.4
-data.raw["map-settings"]["map-settings"].pollution.diffusion_ratio = 0.1
 data.raw["utility-constants"].default.spawner_evolution_factor_health_modifier = 20
 
 data.raw.fire["fire-flame"].initial_lifetime = 300
