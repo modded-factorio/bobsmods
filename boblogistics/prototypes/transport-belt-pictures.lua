98cd3319bc35578e14729aab7ca0083f53b4a37a
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-04-18 18:06:50 +1200

    Added belt hold graphics #409

--- boblogistics/prototypes/transport-belt-pictures.lua
+++ boblogistics/prototypes/transport-belt-pictures.lua
@@ -8,6 +8,110 @@ bobmods.logistics.black_belt_animation_set = {
     frame_count = 16,
     direction_count = 20,
   },
+  belt_reader = {
+    {
+      render_layer = "object",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-top.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "transport-belt-reader",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-base.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor-mechanics",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-middle.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "transport-belt-endings",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-under-middle.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-bottom.png",
+        frame_count = 4,
+        height = 64,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-shadow.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          1,
+          0.5
+        },
+        width = 64
+      }
+    }
+  }
 }
 
 bobmods.logistics.purple_belt_animation_set = {
@@ -20,6 +124,110 @@ bobmods.logistics.purple_belt_animation_set = {
     frame_count = 32,
     direction_count = 20,
   },
+  belt_reader = {
+    {
+      render_layer = "object",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-top.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "transport-belt-reader",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-base.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor-mechanics",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-middle.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "transport-belt-endings",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-under-middle.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-bottom.png",
+        frame_count = 4,
+        height = 64,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-shadow.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          1,
+          0.5
+        },
+        width = 64
+      }
+    }
+  }
 }
 
 bobmods.logistics.green_belt_animation_set = {
@@ -32,4 +240,108 @@ bobmods.logistics.green_belt_animation_set = {
     frame_count = 32,
     direction_count = 20,
   },
+  belt_reader = {
+    {
+      render_layer = "object",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-top.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "transport-belt-reader",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-base.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor-mechanics",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-middle.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "transport-belt-endings",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-under-middle.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0.5
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-bottom.png",
+        frame_count = 4,
+        height = 64,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          0,
+          0
+        },
+        width = 64
+      }
+    },
+    {
+      render_layer = "floor",
+      sprites = {
+        direction_count = 4,
+        filename = "__base__/graphics/entity/transport-belt/belt-reader-shadow.png",
+        frame_count = 4,
+        height = 128,
+        line_length = 4,
+        priority = "low",
+        scale = 0.5,
+        shift = {
+          1,
+          0.5
+        },
+        width = 64
+      }
+    }
+  }
 }
