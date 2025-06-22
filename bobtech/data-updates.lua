987a957f7c46c54b8c189ebeeff817e2b9b7667a
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-06-22 18:38:30 +1200

    Added Transport Science Pack to Biolab #443

--- bobtech/data-updates.lua
+++ bobtech/data-updates.lua
@@ -5,6 +5,9 @@ require("prototypes.item.item-updates")
 if bobmods.tech.advanced_logistic_science then
   bobmods.lib.safe_insert(data.raw["lab"]["lab"].inputs, "bob-advanced-logistic-science-pack", 5)
   bobmods.lib.safe_insert(data.raw["lab"]["bob-lab-2"].inputs, "bob-advanced-logistic-science-pack", 5)
+  if mods["space-age"] then
+    bobmods.lib.safe_insert(data.raw["lab"]["biolab"].inputs, "bob-advanced-logistic-science-pack", 5)
+  end
 end
 
 if mods["quality"] then
