76371d3dda9637f283672ee96e8d9e3b0f684973
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-08-29 17:21:31 +1200

    Lab 2 - accepted science packs #452

--- bobtech/data-updates.lua
+++ bobtech/data-updates.lua
@@ -1,18 +1,4 @@
 require("prototypes.recipe.recipe-updates")
 require("prototypes.technology.technology-updates")
 require("prototypes.item.item-updates")
-
-if bobmods.tech.advanced_logistic_science then
-  bobmods.lib.safe_insert(data.raw["lab"]["lab"].inputs, "bob-advanced-logistic-science-pack", 5)
-  bobmods.lib.safe_insert(data.raw["lab"]["bob-lab-2"].inputs, "bob-advanced-logistic-science-pack", 5)
-  if mods["space-age"] then
-    bobmods.lib.safe_insert(data.raw["lab"]["biolab"].inputs, "bob-advanced-logistic-science-pack", 5)
-  end
-end
-
-if mods["quality"] then
-  bobmods.lib.recipe.update_recycling_recipe_single("lab")
-  if data.raw.recipe["bob-burner-lab"] then
-    bobmods.lib.recipe.update_recycling_recipe_single("bob-burner-lab")
-  end
-end
+require("prototypes.entity.entity-updates")
