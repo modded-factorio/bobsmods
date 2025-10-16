cd24c6358da20dd684ec2f6e44fc28e041a6e967
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-10-15 17:06:45 -0700

    Update data-final-fixes.lua

--- boblogistics/data-final-fixes.lua
+++ boblogistics/data-final-fixes.lua
@@ -7,3 +7,10 @@ if mods["Krastorio2"] then
     data.raw.inserter["long-handed-inserter"].fast_replaceable_group = "inserter"
   end
 end
+
+if mods["aai-industry"] then
+  if data.raw.technology["logistics-0"] then
+    bobmods.lib.tech.add_recipe_unlock("logistics", "transport-belt")
+    bobmods.lib.tech.remove_recipe_unlock("basic-logistics", "transport-belt")
+  end
+end
