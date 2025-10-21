e6b508b549aaf171c5d0ca7dcf51ff23e17c6e27
Qatavin <108201496+Qatavin@users.noreply.github.com>
2025-10-20 21:43:24 -0700

    Update assembly-electronics.lua

--- bobassembly/prototypes/assembly-electronics.lua
+++ bobassembly/prototypes/assembly-electronics.lua
@@ -578,4 +578,48 @@ if settings.startup["bobmods-assembly-electronicmachines"].value == true then
       order = "a-c-c",
     },
   })
+
+  if feature_flags["freezing"] and mods["space-age"] then
+    data.raw["assembling-machine"]["bob-electronics-machine-1"].heating_energy = "50kW"
+    data.raw["assembling-machine"]["bob-electronics-machine-2"].heating_energy = "50kW"
+    data.raw["assembling-machine"]["bob-electronics-machine-3"].heating_energy = "50kW"
+    data.raw["assembling-machine"]["bob-electronics-machine-1"].graphics_set.reset_animation_when_frozen = true
+    data.raw["assembling-machine"]["bob-electronics-machine-2"].graphics_set.reset_animation_when_frozen = true
+    data.raw["assembling-machine"]["bob-electronics-machine-3"].graphics_set.reset_animation_when_frozen = true
+    data.raw["assembling-machine"]["bob-electronics-machine-1"].graphics_set.frozen_patch = {
+      filename = "__space-age__/graphics/entity/frozen/assembling-machine/assembling-machine-1-frozen.png",
+      width = 214,
+      height = 226,
+      priority = "high",
+      scale = 1 / 3,
+      shift = { 0, 0.0625 },
+    }
+    data.raw["assembling-machine"]["bob-electronics-machine-2"].graphics_set.frozen_patch = {
+      filename = "__space-age__/graphics/entity/frozen/assembling-machine/assembling-machine-2-frozen.png",
+      width = 214,
+      height = 218,
+      priority = "high",
+      scale = 1 / 3,
+      shift = { 0, 0.125 },
+    }
+    data.raw["assembling-machine"]["bob-electronics-machine-3"].graphics_set.frozen_patch = {
+      filename = "__space-age__/graphics/entity/frozen/assembling-machine/assembling-machine-3-frozen.png",
+      width = 214,
+      height = 237,
+      priority = "high",
+      scale = 1 / 3,
+      shift = { 0, -0.0234375 },
+    }
+
+    local pipefrozenpatch = data.raw["assembling-machine"]["assembling-machine-2"].fluid_boxes[1].pipe_picture_frozen
+    local pcfrozenpatch = data.raw["assembling-machine"]["assembling-machine-2"].fluid_boxes[1].pipe_covers_frozen
+    data.raw["assembling-machine"]["bob-electronics-machine-1"].fluid_boxes[1].pipe_picture_frozen = pipefrozenpatch
+    data.raw["assembling-machine"]["bob-electronics-machine-1"].fluid_boxes[1].pipe_covers_frozen = pcfrozenpatch
+    data.raw["assembling-machine"]["bob-electronics-machine-2"].fluid_boxes[1].pipe_picture_frozen = pipefrozenpatch
+    data.raw["assembling-machine"]["bob-electronics-machine-2"].fluid_boxes[1].pipe_covers_frozen = pcfrozenpatch
+    data.raw["assembling-machine"]["bob-electronics-machine-3"].fluid_boxes[1].pipe_picture_frozen = pipefrozenpatch
+    data.raw["assembling-machine"]["bob-electronics-machine-3"].fluid_boxes[1].pipe_covers_frozen = pcfrozenpatch
+
+  end
+
 end
