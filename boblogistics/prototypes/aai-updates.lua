22abcaa4ad78b36fea35262a3b8ec6f6440392c2
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-12-26 22:14:17 +1300

    Ordering: New subgroup for vanilla logistics chests #500

--- boblogistics/prototypes/aai-updates.lua
+++ boblogistics/prototypes/aai-updates.lua
@@ -5,8 +5,9 @@ if mods["aai-containers"] then
   data.raw.item["storage-chest"].order = "b[storage]-d"
   data.raw.item["buffer-chest"].order = "b[storage]-e"
   data.raw.item["requester-chest"].order = "b[storage]-f"
-  data.raw["item-subgroup"]["bob-logistic-chests-2"].order = "a1[container-1]-a"
-  data.raw["item-subgroup"]["bob-logistic-chests-3"].order = "a1[container-1]-b"
+  data.raw["item-subgroup"]["bob-logistic-chests-1"].order = "a1[container-1]-a"
+  data.raw["item-subgroup"]["bob-logistic-chests-2"].order = "a1[container-1]-b"
+  data.raw["item-subgroup"]["bob-logistic-chests-3"].order = "a1[container-1]-c"
   if mods["bobplates"] then
     data.raw.item["bob-brass-chest"].subgroup = "bob-logistic-chests-2"
     data.raw.item["bob-titanium-chest"].subgroup = "bob-logistic-chests-3"
