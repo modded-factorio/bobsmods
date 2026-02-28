8a9820b6ce18dd1ca035d32f0c8c08f4fb08aa69
Qatavin <108201496+Qatavin@users.noreply.github.com>
2026-02-27 19:56:21 -0800

    Update burner-reactor.lua

--- bobpower/prototypes/burner-reactor.lua
+++ bobpower/prototypes/burner-reactor.lua
@@ -60,6 +60,7 @@ if settings.startup["bobmods-power-heatsources"].value == true then
         filename = "__base__/sound/item/metal-large-inventory-pickup.ogg",
         volume = 0.8,
       },
+      weight = 50000,
     },
     {
       type = "recipe",
@@ -198,6 +199,7 @@ if settings.startup["bobmods-power-heatsources"].value == true then
         filename = "__base__/sound/item/metal-large-inventory-pickup.ogg",
         volume = 0.8,
       },
+      weight = 50000,
     },
     {
       type = "recipe",
@@ -258,6 +260,7 @@ if settings.startup["bobmods-power-heatsources"].value == true then
         filename = "__base__/sound/item/metal-large-inventory-pickup.ogg",
         volume = 0.8,
       },
+      weight = 50000,
     },
     {
       type = "recipe",
@@ -302,6 +305,7 @@ if settings.startup["bobmods-power-heatsources"].value == true then
         filename = "__base__/sound/item/metal-large-inventory-pickup.ogg",
         volume = 0.8,
       },
+      weight = 50000,
     },
     {
       type = "recipe",
@@ -502,5 +506,15 @@ if settings.startup["bobmods-power-heatsources"].value == true then
     })
 
     bobmods.lib.tech.add_recipe_unlock("bob-burner-reactor-1", "bob-fluid-reactor-from-fluid-furnace")
+  end
+
+  if mods["space-age"] then
+    local no_space = {
+      { property = "pressure", min = 10 },
+    }
+    data.raw.reactor["bob-burner-reactor"].surface_conditions = no_space
+    data.raw.reactor["bob-burner-reactor-2"].surface_conditions = no_space
+    data.raw.reactor["bob-fluid-reactor"].surface_conditions = no_space
+    data.raw.reactor["bob-fluid-reactor-2"].surface_conditions = no_space
   end
 end
