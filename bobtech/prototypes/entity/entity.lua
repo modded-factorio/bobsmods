59f468a04c9815f90a5f9da0c7f17cfa086fe05e
Qatavin <108201496+Qatavin@users.noreply.github.com>
2026-03-28 22:50:22 -0700

    Update entity.lua

--- bobtech/prototypes/entity/entity.lua
+++ bobtech/prototypes/entity/entity.lua
@@ -151,15 +151,17 @@ data:extend({
 
 data.raw["lab"]["lab"].next_upgrade = "bob-lab-2"
 
-if feature_flags["freezing"] and mods["space-age"] then
+if feature_flags["freezing"] then
   data.raw.lab["bob-lab-2"].heating_energy = "100kW"
-  data.raw.lab["bob-lab-2"].frozen_patch = {
-    filename = "__space-age__/graphics/entity/frozen/lab/lab.png",
-    width = 194,
-    height = 174,
-    scale = 0.5,
-    shift = { 0, 0.046875 },
-  }
+  if mods["space-age"] then
+    data.raw.lab["bob-lab-2"].frozen_patch = {
+      filename = "__space-age__/graphics/entity/frozen/lab/lab.png",
+      width = 194,
+      height = 174,
+      scale = 0.5,
+      shift = { 0, 0.046875 },
+    }
+  end
 end
 
 if mods["DiscoScience"] and DiscoScience and DiscoScience.prepareLab then
