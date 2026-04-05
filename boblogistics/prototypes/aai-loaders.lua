b29ba386ffea5f50705021a7bd8b4de07eb2b3ae
Qatavin <108201496+Qatavin@users.noreply.github.com>
2026-04-05 13:44:35 -0700

    Update aai-loaders.lua

--- boblogistics/prototypes/aai-loaders.lua
+++ boblogistics/prototypes/aai-loaders.lua
@@ -8,7 +8,7 @@ then
     regular = { { type = "item", name = "transport-belt", amount = 1 } },
     fast = { { type = "item", name = "fast-transport-belt", amount = 1 } },
     express = { { type = "item", name = "express-transport-belt", amount = 1 } },
-    turbo = { { type = "item", name = "bob-turbo-transport-belt", amount = 1 } },
+    turbo = { { type = "item", name = "turbo-transport-belt", amount = 1 } },
     ultimate = { { type = "item", name = "bob-ultimate-transport-belt", amount = 1 } },
   }
   if settings.startup["bobmods-logistics-inserteroverhaul"].value == true then
@@ -153,7 +153,7 @@ then
 
   AAILoaders.make_tier({
     name = "turbo",
-    transport_belt = "bob-turbo-transport-belt",
+    transport_belt = "turbo-transport-belt",
     color = { 180, 89, 255 },
     fluid = "lubricant",
     fluid_per_minute = 0.25,
