30395160dfcd91bd0df310a4671855a27b5342b6
Qatavin <108201496+Qatavin@users.noreply.github.com>
2026-04-06 16:51:38 -0700

    Update plates.lua

--- bobplates/prototypes/item/plates.lua
+++ bobplates/prototypes/item/plates.lua
@@ -130,18 +130,6 @@ data:extend({
   },
   {
     type = "item",
-    name = "bob-lithium",
-    icon = "__bobplates__/graphics/icons/plate/lithium-plate.png",
-    icon_size = 64,
-    subgroup = "bob-material",
-    order = "c-a-e[bob-lithium-plate]",
-    stack_size = 200,
-    drop_sound = plate_drop_move,
-    inventory_move_sound = plate_drop_move,
-    pick_sound = plate_pick,
-  },
-  {
-    type = "item",
     name = "bob-cobalt-plate",
     icon = "__bobplates__/graphics/icons/plate/cobalt-plate.png",
     icon_size = 64,
@@ -153,3 +141,22 @@ data:extend({
     pick_sound = plate_pick,
   },
 })
+
+if not data.raw.item.lithium then
+  data:extend({
+    {
+      type = "item",
+      name = "lithium",
+      localised_name = { "item-name.bob-lithium" },
+      icon = "__bobplates__/graphics/icons/plate/lithium-plate.png",
+      icon_size = 64,
+      subgroup = "bob-material",
+      order = "c-a-e[bob-lithium-plate]",
+      stack_size = 200,
+      drop_sound = plate_drop_move,
+      inventory_move_sound = plate_drop_move,
+      pick_sound = plate_pick,
+      weight = 500,
+    },
+  })
+end
