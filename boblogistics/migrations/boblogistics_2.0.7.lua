ad9256cb5e79b22d6c414f33db35193e43c416f1
Qatavin <108201496+Qatavin@users.noreply.github.com>
2026-04-06 11:02:00 -0700

    Update boblogistics_2.0.7.lua

--- boblogistics/migrations/boblogistics_2.0.7.lua
+++ boblogistics/migrations/boblogistics_2.0.7.lua
@@ -1,5 +1,5 @@
 for index, force in pairs(game.forces) do
-  if force.technologies["turbo-transport-belt"].researched then
+  if prototypes.technology["turbo-transport-belt"] and force.technologies["turbo-transport-belt"].researched then
     force.technologies["logistics-4"].researched = true
   end
 end
