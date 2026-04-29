1cbd8d7ae3c4940c5d1cf27a5a7824e3d5784cce
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-04-29 20:39:04 +1200

    Weight fix #553

--- bobassembly/prototypes/centrifuge.lua
+++ bobassembly/prototypes/centrifuge.lua
@@ -1,7 +1,7 @@
 if settings.startup["bobmods-assembly-centrifuge"].value == true then
   data.raw.item["centrifuge"].order = "g[centrifuge-1]"
+  data.raw.item["centrifuge"].weight = 1000000
   data.raw["assembling-machine"]["centrifuge"].fast_replaceable_group = "centrifuge"
-  data.raw["assembling-machine"]["centrifuge"].weight = 1000000
 
   data:extend({
     {
