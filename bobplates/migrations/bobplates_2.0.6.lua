4a7fa8a82ba17240e13109b60fe214d33ccf5036
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-05-01 00:12:18 +1200

    Removing migration
    Bob's Lithium processing tech early game (R+G). Having researched this shouldn't research a late game tech.

--- bobplates/migrations/bobplates_2.0.6.lua
+++
@@ -1,5 +0,0 @@
-for index, force in pairs(game.forces) do
-  if force.technologies["bob-lithium-processing"].researched and prototypes.technology["lithium-processing"] then
-    force.technologies["lithium-processing"].researched = true
-  end
-end
