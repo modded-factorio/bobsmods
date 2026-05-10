d04b9ce600f882c4e2eb0365aa191b25cd7407a9
Qatavin <108201496+Qatavin@users.noreply.github.com>
2026-05-10 15:22:17 -0700

    Update storage-tank-recipe-updates.lua

--- boblogistics/prototypes/recipe/storage-tank-recipe-updates.lua
+++ boblogistics/prototypes/recipe/storage-tank-recipe-updates.lua
@@ -1,17 +1,17 @@
+if data.raw.item["bob-zinc-plate"] then
+  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-2", "steel-plate", "bob-zinc-plate")
+  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-2", "steel-plate", "bob-zinc-plate")
+  bobmods.lib.tech.add_prerequisite("bob-fluid-handling-2", "bob-zinc-processing")
+end
+
 if data.raw.item["bob-invar-alloy"] then
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-2", "steel-plate", "bob-invar-alloy")
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-2", "steel-plate", "bob-invar-alloy")
-  bobmods.lib.tech.add_prerequisite("bob-fluid-handling-2", "bob-invar-processing")
+  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-3", "steel-plate", "bob-invar-alloy")
+  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-3", "steel-plate", "bob-invar-alloy")
+  bobmods.lib.tech.add_prerequisite("bob-fluid-handling-3", "bob-invar-processing")
 end
 
 if data.raw.item["bob-steel-pipe"] then
   bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-2", "pipe", "bob-steel-pipe")
-end
-
-if data.raw.item["bob-titanium-plate"] then
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-3", "steel-plate", "bob-titanium-plate")
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-3", "steel-plate", "bob-titanium-plate")
-  bobmods.lib.tech.add_prerequisite("bob-fluid-handling-3", "bob-titanium-processing")
 end
 
 if data.raw.item["bob-titanium-pipe"] then
