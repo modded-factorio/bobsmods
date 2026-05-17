a59dc960820c07989dca53dbec324c56927df212
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-05-17 22:37:02 +1200

    Fix heat source ingredients

--- bobpower/prototypes/burner-reactor-updates.lua
+++ bobpower/prototypes/burner-reactor-updates.lua
@@ -17,10 +17,8 @@ if settings.startup["bobmods-power-heatsources"].value == true then
     bobmods.lib.tech.add_prerequisite("bob-burner-reactor-2", "bob-nickel-processing")
   end
 
-  if data.raw.item["bob-brass-pipe"] then
-    bobmods.lib.recipe.replace_ingredient("bob-fluid-reactor-2", "pipe", "bob-brass-pipe")
-    bobmods.lib.tech.add_prerequisite("bob-burner-reactor-2", "bob-brass-processing")
-  elseif data.raw.item["bob-steel-pipe"] then
-    bobmods.lib.recipe.replace_ingredient("bob-fluid-reactor-2", "pipe", "bob-steel-pipe")
+  if data.raw.item["bob-aluminium-pipe"] then
+    bobmods.lib.recipe.replace_ingredient("bob-fluid-reactor-2", "pipe", "bob-aluminium-pipe")
+    bobmods.lib.tech.add_prerequisite("bob-burner-reactor-2", "bob-aluminium-processing")
   end
 end
