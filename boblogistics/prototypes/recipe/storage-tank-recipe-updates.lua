c5f21f45b47e8656f61bc13b5690d24035a04f2d
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-05-19 17:48:04 +1200

    Recipe updates - storage tanks

--- boblogistics/prototypes/recipe/storage-tank-recipe-updates.lua
+++ boblogistics/prototypes/recipe/storage-tank-recipe-updates.lua
@@ -1,29 +1,33 @@
-if data.raw.item["bob-zinc-plate"] then
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-2", "steel-plate", "bob-zinc-plate")
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-2", "steel-plate", "bob-zinc-plate")
-  bobmods.lib.tech.add_prerequisite("bob-fluid-handling-2", "bob-zinc-processing")
-end
+if mods["bobplates"] then
+  bobmods.lib.recipe.remove_ingredient("storage-tank", "steel-plate")
+  bobmods.lib.recipe.replace_ingredient("storage-tank", "iron-plate", "bob-bronze-alloy")
+  bobmods.lib.recipe.remove_ingredient("bob-storage-tank-all-corners", "steel-plate")
+  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners", "iron-plate", "bob-bronze-alloy")
+  bobmods.lib.tech.add_prerequisite("fluid-handling", "bob-alloy-processing")
 
-if data.raw.item["bob-invar-alloy"] then
+  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-2", "steel-plate", "bob-aluminium-plate")
+  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-2", "steel-plate", "bob-aluminium-plate")
+  bobmods.lib.tech.add_prerequisite("bob-fluid-handling-2", "bob-aluminium-processing")
+
   bobmods.lib.recipe.replace_ingredient("bob-storage-tank-3", "steel-plate", "bob-invar-alloy")
   bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-3", "steel-plate", "bob-invar-alloy")
   bobmods.lib.tech.add_prerequisite("bob-fluid-handling-3", "bob-invar-processing")
-end
 
-if data.raw.item["bob-steel-pipe"] then
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-2", "pipe", "bob-steel-pipe")
-end
-
-if data.raw.item["bob-titanium-pipe"] then
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-3", "pipe", "bob-titanium-pipe")
-end
-
-if data.raw.item["bob-nitinol-alloy"] then
   bobmods.lib.recipe.replace_ingredient("bob-storage-tank-4", "steel-plate", "bob-nitinol-alloy")
   bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-4", "steel-plate", "bob-nitinol-alloy")
   bobmods.lib.tech.add_prerequisite("bob-fluid-handling-4", "bob-nitinol-processing")
-end
 
-if data.raw.item["bob-nitinol-pipe"] then
-  bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-4", "pipe", "bob-nitinol-pipe")
+  if mods["boblogistics"] then
+    bobmods.lib.recipe.add_ingredient("storage-tank", { type = "item", name = "bob-bronze-pipe", amount = 2 })
+    bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners", "pipe", "bob-bronze-pipe")
+
+    bobmods.lib.recipe.add_ingredient("bob-storage-tank-2", { type = "item", name = "bob-aluminium-pipe", amount = 2 })
+    bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-2", "pipe", "bob-aluminium-pipe")
+
+    bobmods.lib.recipe.add_ingredient("bob-storage-tank-3", { type = "item", name = "bob-titanium-pipe", amount = 2 })
+    bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-3", "pipe", "bob-titanium-pipe")
+
+    bobmods.lib.recipe.add_ingredient("bob-storage-tank-4", { type = "item", name = "bob-nitinol-pipe", amount = 2 })
+    bobmods.lib.recipe.replace_ingredient("bob-storage-tank-all-corners-4", "pipe", "bob-nitinol-pipe")
+  end
 end
